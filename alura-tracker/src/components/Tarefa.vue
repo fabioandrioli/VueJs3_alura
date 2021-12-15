b3a7675e2b10f847f8998accfa77e23762e57e45
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2021-12-14 22:29:58 -0300

    Projeto terminado

---
+++ alura-tracker/src/components/Tarefa.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+        <div class="column is-7">
+          {{tarefa.descricao}}
+        </div>
+        <div class="colmn">
+          <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+        </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue'
+  import Cronometro from './Cronometro.vue'
+  import ITarefa from '../interfaces/ITarefa'
+
+  export default defineComponent({
+    props:{
+      tarefa:{
+        type:Object as PropType<ITarefa>,
+        required: true,
+        
+      }
+    },
+    name:"Tarefa",
+    components: { Cronometro }
+  })
+</script>
+<style scoped>
+  .box{
+    background:#FAF0CA;
+  }
+</style>
